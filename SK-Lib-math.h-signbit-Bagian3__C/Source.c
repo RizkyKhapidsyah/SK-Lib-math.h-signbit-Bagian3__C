cfdfc1be9252e5f949cd62bd721f2d8a09d2ab78
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-21 07:55:26 +0700

    Add project files.

---
+++ SK-Lib-math.h-signbit-Bagian3__C/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main(void) {
+    printf("signbit(+0.0) = %d\n", signbit(+0.0));
+    printf("signbit(-0.0) = %d\n", signbit(-0.0));
+
+    _getch();
+    return 0;
+}
